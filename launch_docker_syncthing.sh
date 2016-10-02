e4ce9851f0b50c971110876ec57f2d5acd17975d
George Zero <zero@trove.nyc>
2016-10-01 20:59:50 -0400

    added script to start docker with right options; not started as daemon

---
+++ launch_docker_syncthing.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run --rm --restart=always -p 22000:22000  -p 21027:21027/udp -p 8384:8384 -v ~/src/syncthing/config:/syncthing/config -v ~/src/syncthing/data:/syncthing/data --name syncthing georgezero/rpi-syncthing
